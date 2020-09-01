f153d3090fd48d321b0417b2f7d630c555d4cde9
superbeom <it.super.beom@gmail.com>
2020-09-01 15:29:25 +0900

    Styling a View as a 'Card' Container (with Drop Shadows & Rounded Corners)

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -31,6 +31,17 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.26,
+    shadowRadius: 6,
+    elevation: 6,
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
