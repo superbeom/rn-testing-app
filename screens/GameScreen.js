dc9c5ab50b97807b1d60014b7b55b2b15dae0430
superbeom <it.super.beom@gmail.com>
2020-09-02 12:29:14 +0900

    Switching Between Multiple 'Screens'

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
+
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -16,13 +19,32 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title={"LOWER"} onPress={() => {}} />
+        <Button title={"GREATER"} onPress={() => {}} />
+      </Card>
     </View>
   );
 };
 
-export default GameScreen;
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
 
-const styles = StyleSheet.create({});
+export default GameScreen;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -52,7 +52,10 @@ const StartGameScreen = (props) => {
       <Card style={styles.summaryContainer}>
         <Text>You selected</Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title={"START GAME"} />
+        <Button
+          title={"START GAME"}
+          onPress={props.onStartGame.bind(this, selectedNumber)}
+        />
       </Card>
     );
   }
